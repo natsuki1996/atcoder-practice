97ce24a8df45f224e80ee82ba8212ffa31baa897
natsuki1996 <natsu1580ki@gmail.com>
2020-05-24 17:23:12 +0900

    update abc168-2

--- abc168/C/main.cpp
+++ abc168/C/main.cpp
@@ -9,9 +9,10 @@ const double PI = acos(-1);
 int main() {
   double a, b, h, m;
   cin >> a >> b >> h >> m;
-  double angle_m = m / 60 * 2 * PI;
-  double angle_h = (h / 12 + +m / 720) * 2 * PI;
-  double ans = sqrt(a * a + b * b - 2 * a * b * cos(abs(angle_h - angle_m)));
-  cout << fixed << setprecision(20) << ans << endl;
+  double theta_m = m / 60 * 2 * PI;
+  double theta_h = (h / 12 + +m / 720) * 2 * PI;
+  double ans = sqrt(a * a + b * b - 2 * a * b * cos(abs(theta_h - theta_m)));
+  // cout << fixed << setprecision(20) << ans << endl;
+  printf("%.10f\n", ans);
   return 0;
 }
