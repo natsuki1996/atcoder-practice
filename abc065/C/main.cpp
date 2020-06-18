541e1a171fe93022b6000e23aef750457db5c9a2
natsuki1996 <natsu1580ki@gmail.com>
2020-06-18 12:48:08 +0900

    today bootcamp

--- abc065/C/main.cpp
+++ abc065/C/main.cpp
@@ -4,8 +4,58 @@ using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
+const int mod = 1000000007;
+// const int mod = 998244353;
+struct mint {
+  ll x;  // typedef long long ll;
+  mint(ll x = 0) : x((x % mod + mod) % mod) {}
+  mint operator-() const { return mint(-x); }
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod - a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const { return mint(*this) += a; }
+  mint operator-(const mint a) const { return mint(*this) -= a; }
+  mint operator*(const mint a) const { return mint(*this) *= a; }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod - 2); }
+  mint& operator/=(const mint a) { return *this *= a.inv(); }
+  mint operator/(const mint a) const { return mint(*this) /= a; }
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x; }
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x; }
+
 int main() {
-  int n;
-  cin >> n;
+  int n, m;
+  cin >> n >> m;
+  if (n < m) swap(n, m);
+  if (n - m > 1) {
+    cout << 0 << endl;
+    return 0;
+  }
+
+  mint ans = 1;
+  rep(i, n) {
+    ans *= (i + 1);
+    if (i < m) ans *= (i + 1);
+  }
+  if (n == m) ans *= 2;
+  cout << ans << endl;
   return 0;
 }
