3083380d36b1131dd258ca0efcc9819c65d3afa2
natsuki1996 <natsu1580ki@gmail.com>
2020-06-29 12:30:09 +0900

    today bootcamp

--- contests/diverta2019/C/main.cpp
+++ contests/diverta2019/C/main.cpp
@@ -1,11 +1,27 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
 int main() {
-  ll n;
+  int n;
   cin >> n;
+  vector<string> s(n);
+  rep(i, n) cin >> s[i];
+
+  int c0 = 0, c1 = 0, c2 = 0, c3 = 0;
+  for (auto t : s) {
+    rep(j, t.size()) if (t.substr(j, 2) == "AB") c0++;
+    if (t.front() == 'B' && t.back() == 'A') c1++;
+    if (t.front() == 'B' && t.back() != 'A') c2++;
+    if (t.front() != 'B' && t.back() == 'A') c3++;
+  }
+
+  if (c2 + c3 == 0) {
+    cout << c0 + max(0, c1 - 1) << endl;
+  } else {
+    cout << min(c0 + c1 + c2, c0 + c1 + c3) << endl;
+  }
   return 0;
 }
