db0f4d5941e163eabbcf2bd164d1f209b5be5adb
natsuki1996 <natsu1580ki@gmail.com>
2020-07-03 23:10:40 +0900

    today bootcamp

---
+++ contests/arc066/C/main.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+const int mod = 1000000007;
+// const int mod = 998244353;
+struct mint {
+  ll x;  // typedef long long ll;
+  mint(ll x = 0) : x((x % mod + mod) % mod) {}
+  mint operator-() const { return mint(-x); }
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod - a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const { return mint(*this) += a; }
+  mint operator-(const mint a) const { return mint(*this) -= a; }
+  mint operator*(const mint a) const { return mint(*this) *= a; }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod - 2); }
+  mint& operator/=(const mint a) { return *this *= a.inv(); }
+  mint operator/(const mint a) const { return mint(*this) /= a; }
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x; }
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x; }
+
+int main() {
+  int n, a;
+  cin >> n;
+  map<int, int> mp;
+  rep(i, n) {
+    cin >> a;
+    mp[a]++;
+  }
+
+  bool flag = true;
+  for (int i = (n + 1) % 2 + 2; i <= n - 1; i += 2) {
+    flag &= (mp[i] == 2);
+  }
+
+  if (!flag) {
+    cout << 0 << endl;
+    return 0;
+  }
+
+  mint ans = 1;
+  rep(i, n / 2) ans *= 2;
+  cout << ans << endl;
+  return 0;
+}
---
+++ contests/arc067/C/main.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+const int mod = 1000000007;
+// const int mod = 998244353;
+struct mint {
+  ll x;  // typedef long long ll;
+  mint(ll x = 0) : x((x % mod + mod) % mod) {}
+  mint operator-() const { return mint(-x); }
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod - a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const { return mint(*this) += a; }
+  mint operator-(const mint a) const { return mint(*this) -= a; }
+  mint operator*(const mint a) const { return mint(*this) *= a; }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod - 2); }
+  mint& operator/=(const mint a) { return *this *= a.inv(); }
+  mint operator/(const mint a) const { return mint(*this) /= a; }
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x; }
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x; }
+
+struct Sieve {
+  int n;
+  vector<int> f, primes;
+  Sieve(int n = 1) : n(n), f(n + 1) {
+    f[0] = f[1] = -1;
+    for (ll i = 2; i <= n; ++i) {
+      if (f[i]) continue;
+      primes.push_back(i);
+      f[i] = i;
+      for (ll j = i * i; j <= n; j += i) {
+        if (!f[j]) f[j] = i;
+      }
+    }
+  }
+  bool isPrime(int x) { return f[x] == x; }
+  vector<int> factorList(int x) {
+    vector<int> res;
+    while (x != 1) {
+      res.push_back(f[x]);
+      x /= f[x];
+    }
+    return res;
+  }
+  vector<P> factor(int x) {
+    vector<int> fl = factorList(x);
+    if (fl.size() == 0) return {};
+    vector<P> res(1, P(fl[0], 0));
+    for (int p : fl) {
+      if (res.back().first == p) {
+        res.back().second++;
+      } else {
+        res.emplace_back(p, 1);
+      }
+    }
+    return res;
+  }
+};
+
+int main() {
+  int n;
+  cin >> n;
+  Sieve sieve(n + 1);
+  map<int, int> mp;
+  for (int i = 1; i <= n; i++) {
+    auto f = sieve.factor(i);
+    for (auto x : f) mp[x.first] += x.second;
+  }
+  mint ans = 1;
+  for (auto x : mp) ans *= (x.second + 1);
+  cout << ans << endl;
+  return 0;
+}
