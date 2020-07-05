a5a6bc482ff11c5dd7f98fec27a23081754f6d78
natsuki1996 <natsu1580ki@gmail.com>
2020-07-05 20:59:17 +0900

    today bootcamp

--- contests/abc151/D/main.cpp
+++ contests/abc151/D/main.cpp
@@ -1,11 +1,47 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
+const int INF = 1001001001;
+const vector<int> dx = {-1, 0, 1, 0};
+const vector<int> dy = {0, -1, 0, 1};
+
 int main() {
-  int n;
-  cin >> n;
+  int h, w;
+  cin >> h >> w;
+  vector<string> s(h);
+  rep(i, h) cin >> s[i];
+
+  int ans = 0;
+  rep(si, h) rep(sj, w) {
+    if (s[si][sj] == '#') continue;
+    vector<vector<int>> dist(h, vector<int>(w, INF));
+    queue<P> q;
+
+    auto update = [&](int i, int j, int x) {
+      if (dist[i][j] != INF) return;
+      dist[i][j] = x;
+      q.push(P(i, j));
+    };
+
+    update(si, sj, 0);
+    while (!q.empty()) {
+      int cx = q.front().first, cy = q.front().second;
+      q.pop();
+      rep(i, 4) {
+        int nx = cx + dx[i], ny = cy + dy[i];
+        if (nx < 0 || h <= nx || ny < 0 || w <= ny) continue;
+        if (s[nx][ny] == '#') continue;
+        update(nx, ny, dist[cx][cy] + 1);
+      }
+    }
+    rep(i, h) rep(j, w) {
+      if (dist[i][j] == INF) continue;
+      ans = max(ans, dist[i][j]);
+    }
+  }
+  cout << ans << endl;
   return 0;
 }
--- contests/ddcc2020-qual/C/main.cpp
+++ contests/ddcc2020-qual/C/main.cpp
@@ -1,10 +1,46 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
 int main() {
-  int n;
+  int h, w, k, num = 0;
+  cin >> h >> w >> k;
+  vector<string> s(h);
+  rep(i, h) cin >> s[i];
+
+  vector<vector<int>> a(h, vector<int>(w));
+
+  vector<int> cnt(h);
+  rep(i, h) rep(j, w) if (s[i][j] == '#') cnt[i]++;
+
+  auto solve = [&](int cl, int cr) {
+    vector<int> P;
+    for (int i = cl; i < cr; i++) rep(j, w) if (s[i][j] == '#') P.push_back(j);
+    sort(P.begin(), P.end());
+
+    rep(i, P.size()) {
+      int v1 = 0, v2 = w;
+      if (i >= 1) v1 = P[i - 1] + 1;
+      if (i < P.size() - 1) v2 = P[i] + 1;
+      num++;
+      for (int j = cl; j < cr; j++)
+        for (int k = v1; k < v2; k++) a[j][k] = num;
+    }
+  };
+
+  vector<int> vec;
+  rep(i, h) if (cnt[i] >= 1) vec.push_back(i);
+
+  rep(i, vec.size()) {
+    int v1 = 0, v2 = h;
+    if (i >= 1) v1 = vec[i - 1] + 1;
+    if (i < vec.size() - 1) v2 = vec[i] + 1;
+    solve(v1, v2);
+  }
+
+  rep(i, h) rep(j, w) printf("%d%c", a[i][j], (j == w - 1 ? '\n' : ' '));
+
   return 0;
 }
