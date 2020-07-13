fc2cdf05fd89f9fbfaa02795ac29fe25450d1eae
natsuki1996 <natsu1580ki@gmail.com>
2020-07-13 11:52:15 +0900

    today bootcamp

--- contests/arc067/D/main.cpp
+++ contests/arc067/D/main.cpp
@@ -5,7 +5,16 @@ using ll = long long;
 using P = pair<int, int>;
 
 int main() {
-  int n;
-  cin >> n;
+  ll n, a, b;
+  cin >> n >> a >> b;
+  vector<ll> x(n);
+  rep(i, n) cin >> x[i];
+
+  ll ans = 0;
+  rep(i, n - 1) {
+    ll cost = min((x[i + 1] - x[i]) * a, b);
+    ans += cost;
+  }
+  cout << ans << endl;
   return 0;
 }
