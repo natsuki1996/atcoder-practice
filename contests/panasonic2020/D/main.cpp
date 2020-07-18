b17a91b73775defc2e3be886d47e1b2bef8455f2
natsuki1996 <natsu1580ki@gmail.com>
2020-07-18 16:03:14 +0900

    today bootcamp

--- contests/panasonic2020/D/main.cpp
+++ contests/panasonic2020/D/main.cpp
@@ -1,11 +1,21 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
+ll n;
+
+void solve(string s, char c) {
+  if (s.size() == n) {
+    cout << s << endl;
+    return;
+  }
+  for (char i = 'a'; i <= c; i++) solve(s + i, (i == c ? c + 1 : c));
+}
+
 int main() {
-  int n;
   cin >> n;
+  solve("", 'a');
   return 0;
 }
