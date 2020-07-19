bc0a4333ed33f0ea59207d0c816c9cbc44750649
natsuki1996 <natsu1580ki@gmail.com>
2020-07-19 12:19:13 +0900

    today bootcamp

--- contests/caddi2018/D/main.cpp
+++ contests/caddi2018/D/main.cpp
@@ -1,11 +1,18 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
 int main() {
-  int n;
+  ll n;
   cin >> n;
+  vector<int> a(n);
+  rep(i, n) cin >> a[i];
+
+  bool all_even = true;
+  rep(i, n) if (a[i] % 2 != 0) all_even = false;
+
+  cout << (all_even ? "second" : "first") << endl;
   return 0;
 }
