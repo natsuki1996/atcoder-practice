3064643438c7a55f93693995596b984598e452cb
natsuki1996 <natsu1580ki@gmail.com>
2020-07-23 17:27:50 +0900

    today bootcamp

--- contests/abc157/D/main.cpp
+++ contests/abc157/D/main.cpp
@@ -1,11 +1,60 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
+struct UnionFind {
+  vector<int> d;
+  UnionFind(int n = 0) : d(n, -1) {}
+
+  int find(int x) {
+    if (d[x] < 0) return x;
+    return d[x] = find(d[x]);
+  }
+
+  bool unite(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if (x == y) return false;
+    if (d[x] > d[y]) swap(x, y);
+    d[x] += d[y];
+    d[y] = x;
+    return true;
+  }
+
+  bool same(int x, int y) { return find(x) == find(y); }
+
+  int size(int x) { return -d[find(x)]; }
+};
+
 int main() {
-  ll n;
-  cin >> n;
+  ll n, m, k, a, b, c, d;
+  cin >> n >> m >> k;
+
+  vector<int> deg(n);
+  vector<vector<int>> fr(n), bl(n);
+  UnionFind uf(n);
+  rep(i, m) {
+    cin >> a >> b;
+    a--, b--;
+    fr[a].push_back(b);
+    fr[b].push_back(a);
+    uf.unite(a, b);
+  }
+  rep(i, k) {
+    cin >> c >> d;
+    c--, d--;
+    bl[c].push_back(d);
+    bl[d].push_back(c);
+  }
+
+  rep(i, n) {
+    int ans = uf.size(i) - 1 - fr[i].size();
+    for (auto x : bl[i])
+      if (uf.same(i, x)) ans--;
+    printf("%d%c", ans, (i == n - 1 ? '\n' : ' '));
+  }
+
   return 0;
 }
--- contests/sumitrust2019/E/main.cpp
+++ contests/sumitrust2019/E/main.cpp
@@ -4,8 +4,79 @@ using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
+constexpr const int mod = 1000000007;
+// constexpr const int mod = 998244353;
+class mint {
+ public:
+  long long x;
+  constexpr mint(long long x = 0) noexcept : x((x % mod + mod) % mod) {}
+  constexpr int getmod() { return mod; }
+  constexpr mint operator-() const noexcept { return mint(-x); }
+
+  constexpr mint& operator+=(const mint& a) noexcept {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  constexpr mint& operator-=(const mint& a) noexcept {
+    if ((x += mod - a.x) >= mod) x -= mod;
+    return *this;
+  }
+  constexpr mint& operator*=(const mint& a) noexcept {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  constexpr mint& operator/=(const mint& a) noexcept {
+    return (*this) *= a.inv();
+  }
+
+  constexpr mint operator+(const mint& a) const noexcept {
+    return mint(*this) += a;
+  }
+  constexpr mint operator-(const mint& a) const noexcept {
+    return mint(*this) -= a;
+  }
+  constexpr mint operator*(const mint& a) const noexcept {
+    return mint(*this) *= a;
+  }
+  constexpr mint operator/(const mint& a) const noexcept {
+    return mint(*this) /= a;
+  }
+
+  constexpr mint inv() const noexcept { return pow(mod - 2); }
+  constexpr mint pow(long long t) const noexcept {
+    if (!t) return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  friend istream& operator>>(istream& is, mint& a) noexcept {
+    return is >> a.x;
+  }
+  friend ostream& operator<<(ostream& os, const mint& a) noexcept {
+    return os << a.x;
+  }
+};
+
 int main() {
-  int n;
+  ll n, a;
   cin >> n;
+
+  mint ans = 1;
+  vector<int> d(3);
+  rep(i, n) {
+    cin >> a;
+    int tmp = 0;
+    rep(j, 3) if (d[j] == a) tmp++;
+    rep(j, 3) if (d[j] == a) {
+      d[j]++;
+      break;
+    }
+    ans *= tmp;
+  }
+
+  cout << ans << endl;
+
   return 0;
 }
