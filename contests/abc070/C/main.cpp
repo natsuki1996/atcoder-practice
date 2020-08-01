0a9ef6bdcbd64d0e9ff1aad45c41ec7e5f290424
natsuki1996 <natsu1580ki@gmail.com>
2020-08-01 22:59:33 +0900

    fill ABC

--- contests/abc070/C/main.cpp
+++ contests/abc070/C/main.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using namespace std;
 using ll = long long;
-using P = pair<int, int>;
+using P = pair<ll, ll>;
 
-ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
-ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+const ll INF = 1LL << 62;
+const double PI = acos(-1);
+const double eps = 1e-10;
+
+long long gcd(long long a, long long b) { return b ? gcd(b, a % b) : a; }
+long long lcm(long long a, long long b) { return a / gcd(a, b) * b; }
 
 int main() {
   ll n, t, ans = 1;
