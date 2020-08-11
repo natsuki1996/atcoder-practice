20a24cd03f45178aef1556d19292c4e3576bd6bc
natsuki1996 <natsu1580ki@gmail.com>
2020-08-11 15:52:54 +0900

    today bootcamp

--- contests/abc070/D/main.cpp
+++ contests/abc070/D/main.cpp
@@ -8,8 +8,62 @@ const ll INF = 1LL << 62;
 const double PI = acos(-1);
 const double eps = 1e-10;
 
+struct edge {
+  ll to;
+  ll cost;
+};
+struct graph {
+  vector<vector<edge>> G;
+  vector<ll> d;
+
+  graph(ll n) {
+    G = vector<vector<edge>>(n);
+    d = vector<ll>(n, INF);
+  }
+
+  void add_edge(ll s, ll t, ll cost) {
+    edge e;
+    e.to = t, e.cost = cost;
+    G[s].push_back(e);
+  }
+
+  void dijkstra(ll s) {
+    d[s] = 0;
+    priority_queue<P, vector<P>, greater<P>> q;
+    q.push(P(0, s));
+    while (!q.empty()) {
+      P p = q.top();
+      q.pop();
+      ll v = p.second;
+      if (d[v] < p.first) continue;
+      for (auto e : G[v]) {
+        if (d[e.to] > d[v] + e.cost) {
+          d[e.to] = d[v] + e.cost;
+          q.push(P(d[e.to], e.to));
+        }
+      }
+    }
+  }
+};
+
 int main() {
-  ll n;
+  ll n, a, b, c, q, k, x, y;
   cin >> n;
+  graph g(n);
+  rep(i, n - 1) {
+    cin >> a >> b >> c;
+    a--, b--;
+    g.add_edge(a, b, c);
+    g.add_edge(b, a, c);
+  }
+
+  cin >> q >> k;
+  k--;
+  g.dijkstra(k);
+  rep(i, q) {
+    cin >> x >> y;
+    x--, y--;
+    cout << g.d[x] + g.d[y] << endl;
+  }
   return 0;
 }
