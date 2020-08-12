56d297a169f1acef033cbbb2880c5901246132a6
natsuki1996 <natsu1580ki@gmail.com>
2020-08-13 01:46:03 +0900

    today bootcamp

--- contests/abc147/D/main.cpp
+++ contests/abc147/D/main.cpp
@@ -4,8 +4,22 @@ using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
+constexpr const int mod = 1000000007;
+
 int main() {
   int n;
   cin >> n;
+  vector<ll> a(n);
+  rep(i, n) cin >> a[i];
+
+  ll ans = 0, two_factor = 1;
+  rep(i, 60) {
+    ll even = 0;
+    rep(j, n) if ((a[j] >> i) & 1) even++;
+    ll add = (even * (n - even)) % mod * two_factor % mod;
+    ans = (ans + add) % mod;
+    two_factor = (two_factor * 2) % mod;
+  }
+  cout << ans << endl;
   return 0;
 }
