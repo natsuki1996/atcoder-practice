3dccf8cd423567a62f4a8c5f4100a2d151211b4d
natsuki1996 <natsu1580ki@gmail.com>
2020-08-19 22:38:09 +0900

    today bootcamp

--- contests/apc001/C/main.cpp
+++ contests/apc001/C/main.cpp
@@ -6,6 +6,31 @@ using P = pair<int, int>;
 
 int main() {
   int n;
-  cin >> n;
+  scanf("%d", &n);
+  int l = 0, r = n - 1, m;
+
+  string sl, sr, sm;
+  printf("%d\n", l);
+  fflush(stdout);
+  cin >> sl;
+  printf("%d\n", r);
+  fflush(stdout);
+  cin >> sr;
+  if (sl == "Vacant" || sr == "Vacant") return 0;
+
+  while (l < r) {
+    m = l + (r - l) / 2;
+    printf("%d\n", m);
+    fflush(stdout);
+    cin >> sm;
+    if (sm == "Vacant") return 0;
+    if (((m - l) % 2 == 1 && sl != sm) || ((m - l) % 2 == 0 && sl == sm)) {
+      l = m;
+      sl = sm;
+    } else {
+      r = m;
+      sr = sm;
+    }
+  }
   return 0;
 }
